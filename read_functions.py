7932cd693cd3a70f4f0ef903fdffc299ad0a73bc
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-15 21:31:24 +0400

    implemented a function that reads settings from a .json file. Accepts arguments settings_file (str), and returns a dictionary with settings.

--- read_functions.py
+++ read_functions.py
@@ -5,3 +5,23 @@ from cryptography.hazmat.primitives.serialization import load_pem_private_key
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
+
+
+def read_settings(settings_file: str) -> dict:
+    """Reading settings from a .json file.
+
+    Args:
+        settings_file (str): path to settings file.
+
+    Returns:
+        dict: dictionary with settings.
+    """
+    try:
+        with open(settings_file) as json_file:
+            settings = json.load(json_file)
+        logging.info(
+            f"Settings was successfully read from file{settings_file}!")
+    except OSError as err:
+        logging.warning(
+            f"Settings was not read from file{settings_file}\n{err}")
+    return settings
