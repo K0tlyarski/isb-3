f720fec1f1a36501cf6ff77388bc6c608067d23c
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-15 21:20:00 +0400

    implemented a function that writes text as bytes and accepts arguments text (bytes),file_name (str)

--- write_functions.py
+++ write_functions.py
@@ -4,3 +4,18 @@ from cryptography.hazmat.primitives import serialization
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
+
+
+def byte_write_text(text: bytes, file_name: str) -> None:
+    """Writing text as bytes.
+
+    Args:
+        text (bytes): text for writing.
+        file_name (str): name of .txt file.
+    """
+    try:
+        with open(file_name, "wb") as text_file:
+            text_file.write(text)
+        logging.info(f"Text was successfully written to file {file_name}!")
+    except OSError as err:
+        logging.warning(f"Text was not written to file {file_name}\n{err}!")
