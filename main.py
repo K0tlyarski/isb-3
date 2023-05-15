4e7ded61b6dad0416ced507e06bb97f0867211fb
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-15 21:48:02 +0400

    starting the implementation

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ from write_functions import (byte_write_text, write_private_key)
 
 SETTINGS_FILE = "files/settings.json"
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required = True)
+    group.add_argument('-gen','--generation',help='Запускает режим генерации ключей')
+    group.add_argument('-enc','--encryption',help='Запускает режим шифрования')
+    group.add_argument('-dec','--decryption',help='Запускает режим дешифрования')
 
-parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group(required = True)
-group.add_argument('-gen','--generation',help='Запускает режим генерации ключей')
-group.add_argument('-enc','--encryption',help='Запускает режим шифрования')
-group.add_argument('-dec','--decryption',help='Запускает режим дешифрования')
-
-args = parser.parse_args()
-if args.generation is not None:
-  # генерируем ключи
-else if args.encryption is not None:
-  # шифруем
-else:
-  # дешифруем
+    args = parser.parse_args()
+    if args.generation is not None:
+      # генерируем ключи
+    else if args.encryption is not None:
+      # шифруем
+    else:
+      # дешифруем
