99e9ff397848d9e35ca278ae5e24ad30ec569f12
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-15 21:54:30 +0400

    implementation of key generation, encryption and decryption

--- main.py
+++ main.py
@@ -16,14 +16,31 @@ if __name__ == "__main__":
                              "необходимых настроек"
                              "(Введите путь к файлу)")
     group = parser.add_mutually_exclusive_group(required = True)
-    group.add_argument('-gen','--generation',help='Запускает режим генерации ключей')
-    group.add_argument('-enc','--encryption',help='Запускает режим шифрования')
-    group.add_argument('-dec','--decryption',help='Запускает режим дешифрования')
+    group.add_argument('-gen','--generation',
+                       help='Запускает режим генерации ключей', action="store_true")
+    group.add_argument('-enc','--encryption',
+                       help='Запускает режим шифрования', action="store_true")
+    group.add_argument('-dec','--decryption',
+                       help='Запускает режим дешифрования', action="store_true")
 
     args = parser.parse_args()
-    if args.generation is not None:
-      # генерируем ключи
-    else if args.encryption is not None:
-      # шифруем
+    settings = read_settings(args.settings)
+    if args.generation:
+        private_key, public_key = asymmetric_keys_generation()
+        symmetric_key = symmetric_key_generation()
+        write_private_key(private_key, settings["private_key"])
+        encrypted_symmetric_key = asymmetric_key_encryption(
+            public_key, symmetric_key)
+        byte_write_text(encrypted_symmetric_key, settings["symmetric_key"])
+    elif args.encryption:
+        symmetric_key = asymmetric_key_decryption(read_private_key(
+            settings["private_key"]), byte_read_text(settings["symmetric_key"]))
+        text = byte_read_text(settings["text_file"])
+        cipher_text = symmetric_encryption(symmetric_key, text)
+        byte_write_text(cipher_text, settings["encrypted_file"])
     else:
-      # дешифруем
+        symmetric_key = asymmetric_key_decryption(read_private_key(
+            settings["private_key"]), byte_read_text(settings["symmetric_key"]))
+        text = symmetric_decryption(
+            symmetric_key, byte_read_text(settings["encrypted_file"]))
+        byte_write_text(text, settings["decrypted_file"])
