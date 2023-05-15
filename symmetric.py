c13594af1cd55fb89ad6ddd6c97407d1b3d74e99
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-15 20:39:22 +0400

    the key generation function for the asymmetric encryption algorithm is implemented

--- symmetric.py
+++ symmetric.py
@@ -3,3 +3,17 @@ import os
 
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+
+def symmetric_key_generation() -> bytes:
+    """Generating a key for a symmetric encryption algorithm
+    Returns:
+        bytes: key for symmetric encryption algorithm.
+    """
+    key = os.urandom(16)
+    logging.info("Symmetric key successfully generated!")
+    return key
