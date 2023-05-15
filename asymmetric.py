3e6a1c6cd53bddc5ada2b8bd7717308ec1f4faa0
K0tlyarski <djgame2.9.6@gmail.com>
2023-05-15 21:00:29 +0400

    implemented a function that generates private and public keys, which returns a tuple with private and public keys.

--- asymmetric.py
+++ asymmetric.py
@@ -2,3 +2,22 @@ import logging
 
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+
+def asymmetric_keys_generation() -> tuple:
+    """Private and public key generation.
+
+    Returns:
+        tuple: tuple with private and public keys.
+    """
+    keys = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    private_key = keys
+    public_key = keys.public_key()
+    logging.info(f"Asymmetric keys successfully generated!")
+    return (private_key, public_key)
